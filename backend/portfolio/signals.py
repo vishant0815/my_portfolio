4138f0051bdeedd9b52e0cb15ecc786d8fdaab1a
vishant <vishantbhavsar500@gmail.com>
2025-06-24 11:24:34 +0530

    Update superuser details

--- backend/portfolio/signals.py
+++ backend/portfolio/signals.py
@@ -6,9 +6,9 @@ import os
 @receiver(post_migrate)
 def create_default_superuser(sender, **kwargs):
     User = get_user_model()
-    username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
-    email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
-    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin123")
+    username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "chahu")
+    email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "chahatvishant1508@gmail.com")
+    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "Ch@h@t_1508")
 
     if not User.objects.filter(username=username).exists():
         User.objects.create_superuser(username=username, email=email, password=password)
