4a0490b5b15e2f3e7c11f37bf21d32e70b51e0de
vishant <vishantbhavsar500@gmail.com>
2025-06-24 14:43:56 +0530

    update settings file

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -140,7 +140,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "https://my-portfolio-dusky-eight-69.vercel.app",
+    "https://my-portfolio-k5b2g64p0-vishants-projects-d5adbb8f.vercel.app",
 ]
 
 STATIC_URL = '/static/'
