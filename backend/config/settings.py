34fe3744beee71a05838c6d779c7ac532d00cca2
vishant <vishantbhavsar500@gmail.com>
2025-06-24 13:22:44 +0530

    Link backend to the frontend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -139,7 +139,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "my-portfolio-dusky-eight-69.vercel.app",
+]
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
